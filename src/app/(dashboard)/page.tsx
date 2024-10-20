fdfde247ff6ef2a33424585725e1d963b864921f
ArtycoBW <artyco.bw@gmail.com>
2024-10-20 11:50:03 +0300

    создание макета боковой панели

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { getCurrent } from '@/features/auth/actions'
-import UserButton from '@/features/auth/components/user-button'
 import { redirect } from 'next/navigation'
 
 export default async function Home() {
@@ -7,9 +6,5 @@ export default async function Home() {
 
   if (!user) await redirect('/sign-in')
 
-  return (
-    <>
-      <UserButton />
-    </>
-  )
+  return <>Это домашняя страница</>
 }
