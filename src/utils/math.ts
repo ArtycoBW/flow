ce459eb01446b4d5f0ed266eca74249878e39339
ArtycoBW <artyco.bw@gmail.com>
2024-11-10 00:20:25 +0300

    добавление тестов

---
+++ src/utils/math.ts
@@ -0,0 +1,9 @@
+/**
+ * Функция для сложения двух чисел.
+ * @param a - первое число
+ * @param b - второе число
+ * @returns сумма a и b
+ */
+export function add(a: number, b: number): number {
+  return a + b
+}
