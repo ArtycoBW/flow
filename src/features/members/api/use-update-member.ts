59be74893c804b75aaac7b11031003b017ff32f6
ekaterina.shevchenko <ekaterina.shevchenko@axenix.pro>
2025-01-12 18:39:28 +0300

    8:10:05: Adding delete/update functionality

---
+++ src/features/members/api/use-update-member.ts
@@ -0,0 +1,32 @@
+import { client } from '@/lib/rpc'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { InferRequestType, InferResponseType } from 'hono'
+import { toast } from 'sonner'
+
+type ResponseType = InferResponseType<(typeof client.api.members)[':memberId']['$patch'], 200>
+type RequestType = InferRequestType<(typeof client.api.members)[':memberId']['$patch']>
+
+export const useUpdateMember = () => {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param, json }) => {
+      const response = await client.api.members[':memberId']['$patch']({ param, json })
+
+      if (!response.ok) {
+        throw new Error('Произошла ошибка при обновлении участника')
+      }
+
+      return await response.json()
+    },
+    onSuccess: () => {
+      toast.success('Участник успешно обновлен')
+      queryClient.invalidateQueries({ queryKey: ['members'] })
+    },
+    onError: () => {
+      toast.error('Произошла ошибка при обновлении участника')
+    },
+  })
+
+  return mutation
+}
